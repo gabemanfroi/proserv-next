d4f6e730f8d9570ed1c4b9814a8b8878feb3367c
Gabriel Manfroi <gabriel.manfroi@edreamsodigeo.com>
2023-08-29 07:16:30 +0100

    :iphone: Fixes responsiveness
    fixes responsiveness

--- src/components/Home/Sections/Contact/index.js
+++ src/components/Home/Sections/Contact/index.js
@@ -2,13 +2,11 @@
 import {
   MdEmail,
   MdLocationOn,
-  MdOutlineFacebook,
   MdOutlineWhatsapp,
   MdPhone,
 } from "react-icons/md";
 import { ContactItem } from "@/components/Home/Sections/Contact/Contact.item";
 import { ContactForm } from "@/components/Home/Sections/Contact/Contact.form";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { ContactItemDescription } from "@/components/Home/Sections/Contact/Contact.item.description";
 
@@ -16,7 +14,7 @@ const CompanyInfo = () => {
   const router = useRouter();
 
   return (
-    <div className="flex-col flex gap-4 pl-24">
+    <div className="flex-col flex gap-4 lg:pl-24 content-center">
       <ContactItem.Root>
         <ContactItem.Icon icon={MdLocationOn} />
         <ContactItemDescription>
@@ -42,13 +40,6 @@ const CompanyInfo = () => {
           <p>+55 (46) 3225-3314</p>
         </ContactItem.Description>
       </ContactItem.Root>
-      <div className="flex mt-2">
-        <Link href={"https://www.facebook.com/proservpb"} target={"_blank"}>
-          <ContactItem.Root>
-            <ContactItem.Icon icon={MdOutlineFacebook} color={"white"} />
-          </ContactItem.Root>
-        </Link>
-      </div>
     </div>
   );
 };
@@ -56,14 +47,14 @@ const CompanyInfo = () => {
 export const Contact = () => {
   return (
     <section className="w-full bg-black flex justify-center ">
-      <div className="h-full w-full p-36 flex flex-col gap-24">
+      <div className="h-full w-full lg:p-36 p-8 flex flex-col gap-24">
         <div>
           <h2 className="text-center font-bold text-4xl uppercase mb-2">
             Contato
           </h2>
           <h3 className="text-center">Entre em contato conosco agora mesmo!</h3>
         </div>
-        <div className="grid-cols-2 inline-grid gap-8 justify-center px-80">
+        <div className="lg:grid-cols-2 lg:inline-grid flex flex-col md:px-56 gap-8 justify-center lg:px-80">
           <CompanyInfo />
           <ContactForm />
         </div>
