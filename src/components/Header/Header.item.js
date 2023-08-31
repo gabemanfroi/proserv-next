b2db9fe2ad0687744d27e6142b0be42575489211
Gabriel Manfroi <gabriel.manfroi@edreamsodigeo.com>
2023-08-31 20:24:18 +0100

    :art: fixing redundant classes

--- src/components/Header/Header.item.js
+++ src/components/Header/Header.item.js
@@ -3,7 +3,7 @@ export const HeaderItem = ({ href, text }) => {
     <li>
       <a
         href={href}
-        className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 hover:text-amber-400 md:p-0 dark:text-white    md:dark:hover:bg-transparent"
+        className="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:border-0 hover:text-amber-400 md:p-0 text-white md:hover:bg-transparent"
         aria-current="page"
       >
         {text}
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,12 +4,12 @@ import { HeaderMenu } from "@/components/Header/Header.menu";
 export function Header() {
   return (
     <nav
-      className="bg-blue-950 border-gray-200 dark:bg-blue-950 absolute w-screen flex"
+      className="bg-blue-950 border-gray-200  absolute w-screen flex"
       style={{ zIndex: 2, height: "80px" }}
     >
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 flex-1">
         <a href="https://flowbite.com/" className="flex items-center">
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white hover:text-amber-400">
+          <span className="self-center text-2xl font-semibold whitespace-nowrap text-white hover:text-amber-400">
             Proserv & Solution Informática
           </span>
         </a>
